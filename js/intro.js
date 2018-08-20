c1bd9408b985a76c791c2373de7beb54f16e0a0a
Pavel Kreis <kreislab@gmail.com>
2018-08-20 23:16:32 +0300

    Проблема с отработкой нажатий

---
+++ js/intro.js
@@ -0,0 +1,18 @@
+import {getElementFromTemplate, changeScreen} from './createdom.js';
+import greeting from './greeting.js';
+
+const template = `
+<section class="intro">
+<button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+<p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+</section>`;
+
+const intro = getElementFromTemplate(template);
+
+const asterisk = intro.querySelector(`.intro__asterisk`);
+asterisk.addEventListener(`click`, () => {
+  changeScreen(greeting);
+});
+
+
+export default intro;
