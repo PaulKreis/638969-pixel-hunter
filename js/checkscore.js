b020bbb58f51b3a45a9f4fe77d94e376d27aabc4
Pavel Kreis <kreislab@gmail.com>
2018-08-31 18:27:40 +0300

    Выполнено задание 2

---
+++ js/checkscore.js
@@ -0,0 +1,19 @@
+export const checkscore = (answers, lifes) => {
+  let scores = 0;
+  if (answers.length < 10) {
+    return -1;
+  }
+  answers.forEach((element) => {
+    if (element.correct === true) {
+      scores += 100;
+      if (element.time < 10) {
+        scores += 50;
+      }
+    }
+    if (element.time > 20) {
+      scores -= 50;
+    }
+  });
+  scores += lifes * 50;
+  return scores;
+};
